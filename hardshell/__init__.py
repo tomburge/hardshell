6f2047cf0f9fb21e7cf2d216b498e682d2be5986
Tom Burge <tom@tomburge.org>
2023-09-14 08:56:06 -0500

    added poetry

---
+++ hardshell/__init__.py
@@ -0,0 +1,3 @@
+# hardshell/__init__.py
+
+__version__ = "0.1.0"
---
+++ hardshell/__main__.py
@@ -0,0 +1,31 @@
+import click
+from hardshell import __version__
+
+@click.version_option(version=__version__)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+def audit():
+    click.echo("audit")
+
+
+@click.command()
+def harden():
+    click.echo("harden")
+
+
+cli.add_command(audit)
+cli.add_command(harden)
+
+
+def main():
+    cli()
+
+
+if __name__ == "__main__":
+    main()
