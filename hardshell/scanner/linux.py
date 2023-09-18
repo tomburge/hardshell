d701a70b8cf81e7a81701edb2aeb2fdb754db8db
Tom Burge <tom@tomburge.org>
2023-09-18 10:14:23 -0500

    finished filesystem checks

--- hardshell/scanner/linux.py
+++ hardshell/scanner/linux.py
@@ -10,7 +10,9 @@ def kernel_module_loadable(mode, config, mod_type, mod_name):
     if mode == "audit":
         if disable:
             # cmd = f"echo 'install /bin/false' >> {mod_type}-{mod_name}.conf"
-            cmd = f"echo 'install /bin/false\n' >> {mp_config}{mod_type}-{mod_name}.conf"
+            cmd = (
+                f"echo 'install /bin/false\n' >> {mp_config}{mod_type}-{mod_name}.conf"
+            )
             try:
                 result = subprocess.run(
                     cmd, shell=True, check=True, capture_output=True, text=True
@@ -63,9 +65,7 @@ def kernel_module_deny(mode, config, mod_type, mod_name):
     if mode == "audit":
         if disable:
             # cmd = f"echo 'blacklist {mod_name}' >> {mod_type}-{mod_name}.conf"
-            cmd = (
-                f"echo 'blacklist {mod_name}\n' >> {mp_config}{mod_type}-{mod_name}.conf"
-            )
+            cmd = f"echo 'blacklist {mod_name}\n' >> {mp_config}{mod_type}-{mod_name}.conf"
             try:
                 result = subprocess.run(
                     cmd, shell=True, check=True, capture_output=True, text=True
