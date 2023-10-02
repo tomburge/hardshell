3f3e9c3a3d7a8187ec1de3444ccc444c84e21333
Tom Burge <tom@tomburge.org>
2023-10-02 09:12:56 -0500

    fingers crossed

--- hardshell/scanner/linux/common.py
+++ hardshell/scanner/linux/common.py
@@ -6,7 +6,9 @@ import click
 
 def run_command(command):
     try:
-        result = subprocess.run(command, capture_output=True, check=True, text=True)
+        result = subprocess.run(
+            command, capture_output=True, check=True, shell=True, text=True
+        )
         click.echo(result)
         click.echo(result.stdout)
         return result
--- hardshell/scanner/linux/system_check.py
+++ hardshell/scanner/linux/system_check.py
@@ -183,10 +183,10 @@ def check_command(config, category, sub_category, check):
     check_cmd = config[category][sub_category][check]["command"]
     click.echo(check_name)
     # click.echo(check_cmd)
-    result = subprocess.run(
-        check_cmd, capture_output=True, check=True, shell=True, text=True
-    )
-    # result = run_command(check_cmd)
+    # result = subprocess.run(
+    #     check_cmd, capture_output=True, check=True, shell=True, text=True
+    # )
+    result = run_command(check_cmd)
     click.echo(result)
 
 
