bd7797c99e427bb6a2ac3293e56ee03e5a4b5f01
Tom Burge <tom@tomburge.org>
2023-10-02 09:36:58 -0500

    testing fail again

--- hardshell/scanner/linux/system_check.py
+++ hardshell/scanner/linux/system_check.py
@@ -203,6 +203,14 @@ def check_command(config, category, sub_category, check):
             status_color="bright_red",
             log_level="info",
         )
+    else:
+        log_status(
+            " " * 4 + f"- [CHECK] - {check_name}: {check_setting}",
+            message_color="blue",
+            status="FAIL",
+            status_color="bright_red",
+            log_level="info",
+        )
 
 
 # Harden Functions
