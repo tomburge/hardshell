d19ea84da2eb4d05299c49a998ba33efb2890007
Tom Burge <tom@tomburge.org>
2023-10-16 10:43:13 -0500

    removed error checking

--- hardshell/scanner/linux/common.py
+++ hardshell/scanner/linux/common.py
@@ -9,33 +9,33 @@ def run_command(command):
         # click.echo(f"command: {command}")
         if type(command) == list:
             command = " ".join(command)
-        click.echo(f"command: {command}")
+        # click.echo(f"command: {command}")
         result = subprocess.run(
             command, capture_output=True, check=True, shell=True, text=True
         )
-        click.echo(result)
+        # click.echo(result)
         # click.echo(result.stdout)
         # click.echo(result.stderr)
         return result.stdout
     except subprocess.CalledProcessError as error:
-        click.echo(f"sp error: {error}")
-        click.echo(f"sp error returncode: {error.returncode}")
-        click.echo(f"sp error cmd: {error.cmd}")
-        click.echo(f"sp error stderr: {error.stderr}")
-        click.echo(f"sp error output: {error.output}")
-        click.echo(f"sp error type: {type(error)}")
-        click.echo(f"sp error output type: {type(error.output)}")
+        # click.echo(f"sp error: {error}")
+        # click.echo(f"sp error returncode: {error.returncode}")
+        # click.echo(f"sp error cmd: {error.cmd}")
+        # click.echo(f"sp error stderr: {error.stderr}")
+        # click.echo(f"sp error output: {error.output}")
+        # click.echo(f"sp error type: {type(error)}")
+        # click.echo(f"sp error output type: {type(error.output)}")
         return error.stderr
         # return error.output
 
     except Exception as error:
-        click.echo(f"error: {error}")
-        click.echo(f"error returncode: {error.returncode}")
-        click.echo(f"error cmd: {error.cmd}")
-        click.echo(f"error stderr: {error.stderr}")
-        click.echo(f"error output: {error.output}")
-        click.echo(f"error type: {type(error)}")
-        click.echo(f"error output type: {type(error.output)}")
+        # click.echo(f"error: {error}")
+        # click.echo(f"error returncode: {error.returncode}")
+        # click.echo(f"error cmd: {error.cmd}")
+        # click.echo(f"error stderr: {error.stderr}")
+        # click.echo(f"error output: {error.output}")
+        # click.echo(f"error type: {type(error)}")
+        # click.echo(f"error output type: {type(error.output)}")
         return error.with_traceback()
         # return error.output
 
