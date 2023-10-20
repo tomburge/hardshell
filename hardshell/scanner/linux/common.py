6b19e82c95ce4b095c1617bcccc8f629b98d2167
Tom Burge <tom@tomburge.org>
2023-10-19 22:46:49 -0500

    maybe working now?

--- hardshell/scanner/linux/common.py
+++ hardshell/scanner/linux/common.py
@@ -113,30 +113,16 @@ def get_permissions(path):
         return error.output
 
 
-# def run_regex(file_path, pattern):
-#     # Decode the pattern if needed (depending on how it's stored in TOML)
-#     decoded_pattern = pattern.encode().decode("unicode_escape")
-
-#     print(f"Checking file: {file_path} with pattern: {decoded_pattern}")
-
-#     with open(file_path, "r") as file:
-#         for line_num, line in enumerate(file, 1):
-#             print(f"Checking line {line_num}: {line.strip()}")
-#             if re.search(decoded_pattern, line.strip()):
-#                 print(f"Match found on line {line_num}: {line.strip()}")
-#                 return True  # If a match is found, return True
-
-#     return False  # Return False if no matches after iterating through the file
-
-
 def run_regex(file_path, pattern):
     with open(file_path, "r") as file:
         for line_num, line in enumerate(file, 1):
             stripped_line = line.strip()
             match = re.match(pattern, stripped_line)
             if match:
-                return True, f"Matched line {line_num}: {stripped_line}"
-    return False, "No match found."
+                return True
+                # return True, f"Matched line {line_num}: {stripped_line}"
+    return False
+    # return False, "No match found."
 
 
 # def run_regex(file, pattern):
